350a74023416a28179083863c396c07c60dd7dd4
Ashik <gutiashik@gmail.com>
2026-09-08 15:14:13 +0600

    Do not track build-linux/: the out-of-source Linux build directory
    .gitignore covered build/ and build_*/ but not the build-<platform>/ naming the
    Linux build uses, so a whole build tree was committed.

--- build-linux/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover
-# Build directory: /mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover/build-linux
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tools/benchmark")
-subdirs("tests")
--- build-linux/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover/tests
-# Build directory: /mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover/build-linux/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[unit]=] "/mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover/build-linux/bin/promatte-unit-tests")
-set_tests_properties([=[unit]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover/tests/CMakeLists.txt;26;add_test;/mnt/d/Hakeemify/Customer Analysis/OBS Plugin/Background Remover/tests/CMakeLists.txt;0;")
